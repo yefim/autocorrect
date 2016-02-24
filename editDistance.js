e27dca8ad3cc8c72f22aeadff0f798c2dd2975fc
Geoffrey Vedernikoff <yefim323@gmail.com>
2016-02-24 02:14:42 -0800

    Speed it up

--- editDistance.js
+++ editDistance.js
@@ -3,11 +3,15 @@ var editDistance = function(a, b) {
     return Math.max(a.length, b.length);
   }
 
-  return +(a[0] !== b[0]) + Math.min(
-    editDistance(a.substring(1), b.substring(1)),
-    editDistance(a.substring(1), b),
-    editDistance(a, b.substring(1))
-  );
+  if (a[0] === b[0]) {
+    return editDistance(a.substring(1), b.substring(1));
+  } else {
+    return 1 + Math.min(
+      editDistance(a.substring(1), b.substring(1)),
+      editDistance(a.substring(1), b),
+      editDistance(a, b.substring(1))
+    );
+  }
 };
 
 module.exports = editDistance;
