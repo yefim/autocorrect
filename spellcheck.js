f6ff449d7eff2be857b727b6bea41d1da7915971
Geoffrey Vedernikoff <yefim323@gmail.com>
2016-02-24 14:55:11 -0800

    Remove all semicolons

--- spellcheck.js
+++ spellcheck.js
@@ -1,24 +1,24 @@
-var fs = require('fs');
-var editDistance = require('./editDistance');
+var fs = require('fs')
+var editDistance = require('./editDistance')
 
-var dictionary = fs.readFileSync('/usr/share/dict/words').toString().trim().split('\n');
+var dictionary = fs.readFileSync('/usr/share/dict/words').toString().trim().split('\n')
 
 var spellcheck = function(str) {
-  var distance;
-  var bestWord;
-  var min = Infinity;
+  var distance
+  var bestWord
+  var min = Infinity
 
   for (var i = 0, l = dictionary.length; i < l; i++) {
-    distance = editDistance(str, dictionary[i]);
+    distance = editDistance(str, dictionary[i])
     if (distance === 0) {
-      return dictionary[i];
+      return dictionary[i]
     } else if (distance < min) {
-      min = distance;
-      bestWord = dictionary[i];
+      min = distance
+      bestWord = dictionary[i]
     }
   }
 
-  return bestWord;
-};
+  return bestWord
+}
 
-module.exports = spellcheck;
+module.exports = spellcheck
--- test/test.js
+++ test/test.js
@@ -1,54 +1,54 @@
-var assert = require('assert');
-var spellcheck = require('../spellcheck');
+var assert = require('assert')
+var spellcheck = require('../spellcheck')
 
 describe('spellcheck', function() {
-  this.timeout(6000);
+  this.timeout(6000)
 
   describe('matching words', function() {
     it('should return arm', function() {
-      assert.equal(spellcheck('arm'), 'arm');
-    });
+      assert.equal(spellcheck('arm'), 'arm')
+    })
 
     it('should return bob', function() {
-      assert.equal(spellcheck('bob'), 'bob');
-    });
+      assert.equal(spellcheck('bob'), 'bob')
+    })
 
     it('should return zebra', function() {
-      assert.equal(spellcheck('zebra'), 'zebra');
-    });
-  });
+      assert.equal(spellcheck('zebra'), 'zebra')
+    })
+  })
 
   describe('one letter swapped', function() {
     it('should return adz', function() {
-      assert.equal(spellcheck('arz'), 'adz');
-    });
+      assert.equal(spellcheck('arz'), 'adz')
+    })
 
     it('should return at', function() {
-      assert.equal(spellcheck('zt'), 'at');
-    });
-  });
+      assert.equal(spellcheck('zt'), 'at')
+    })
+  })
 
   describe('two letters swapped', function() {
     it('should return aardvark', function() {
-      assert.equal(spellcheck('bordvark'), 'aardvark');
-    });
-  });
+      assert.equal(spellcheck('bordvark'), 'aardvark')
+    })
+  })
 
   describe('one letter to add', function() {
     it('should return embryo', function() {
-      assert.equal(spellcheck('mbryo'), 'embryo');
-    });
-  });
+      assert.equal(spellcheck('mbryo'), 'embryo')
+    })
+  })
 
   describe('two letters to add', function() {
     it('should return embryonic', function() {
-      assert.equal(spellcheck('mbrynic'), 'embryonic');
-    });
-  });
+      assert.equal(spellcheck('mbrynic'), 'embryonic')
+    })
+  })
 
   describe('three letters to add', function() {
     it('should return zymophosphate', function() {
-      assert.equal(spellcheck('zmoposphat'), 'zymophosphate');
-    });
-  });
-});
+      assert.equal(spellcheck('zmoposphat'), 'zymophosphate')
+    })
+  })
+})
