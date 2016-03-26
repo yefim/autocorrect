cebf03be6805b6737789e7108a5aeafd0a496290
Geoffrey Vedernikoff <yefim323@gmail.com>
2016-03-25 20:46:19 -0700

    Improved tests

--- test/test.js
+++ test/test.js
@@ -1,59 +1,6 @@
 var assert = require('assert')
-var autocorrect = require('../index')()
 var framecomplete = require('../index')({dictionary: __dirname + '/dict.txt'})
 var wordcomplete = require('../index')({words: ['word', 'words', 'weird']})
-
-describe('autocorrect', function() {
-  this.timeout(6000)
-
-  describe('matching words', function() {
-    it('should return arm', function() {
-      assert.equal(autocorrect('arm'), 'arm')
-    })
-
-    it('should return bob', function() {
-      assert.equal(autocorrect('bob'), 'bob')
-    })
-
-    it('should return zebra', function() {
-      assert.equal(autocorrect('zebra'), 'zebra')
-    })
-  })
-
-  describe('one letter swapped', function() {
-    it('should return adz', function() {
-      assert.equal(autocorrect('arz'), 'adz')
-    })
-
-    it('should return at', function() {
-      assert.equal(autocorrect('zt'), 'at')
-    })
-  })
-
-  describe('two letters swapped', function() {
-    it('should return aardvark', function() {
-      assert.equal(autocorrect('bordvark'), 'aardvark')
-    })
-  })
-
-  describe('one letter to add', function() {
-    it('should return embryo', function() {
-      assert.equal(autocorrect('mbryo'), 'embryo')
-    })
-  })
-
-  describe('two letters to add', function() {
-    it('should return embryonic', function() {
-      assert.equal(autocorrect('mbrynic'), 'embryonic')
-    })
-  })
-
-  describe('three letters to add', function() {
-    it('should return zymophosphate', function() {
-      assert.equal(autocorrect('zmoposphat'), 'zymophosphate')
-    })
-  })
-})
 
 describe('framecomplete', function() {
   describe('matching words', function() {
@@ -73,6 +20,24 @@ describe('framecomplete', function() {
   describe('one letter swapped', function() {
     it('should return fro', function() {
       assert.equal(framecomplete('fra'), 'fro')
+    })
+  })
+
+  describe('two letters swapped', function() {
+    it('should return fro', function() {
+      assert.equal(framecomplete('ooo'), 'fro')
+    })
+  })
+
+  describe('one letter to add', function() {
+    it('should return fro', function() {
+      assert.equal(framecomplete('fr'), 'fro')
+    })
+  })
+
+  describe('two letters to add', function() {
+    it('should return ear', function() {
+      assert.equal(framecomplete('e'), 'ear')
     })
   })
 })
